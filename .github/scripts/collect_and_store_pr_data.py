eeabd0230762f70c04f0fa40d02717763c3e269d
Kishore Kumar Behera <kishoree.behera2010@gmail.com>
2023-03-18 06:31:59 +0000

    Added github action scripts

---
+++ .github/scripts/collect_and_store_pr_data.py
@@ -0,0 +1,30 @@
+import os
+import json
+import requests
+
+def send_to_slack(pr_title, pr_author, pr_merged_at, pr_url, pr_commit_hash, webhook_url):
+    message = {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*PR Title:* {pr_title}\n*Author:* {pr_author}\n*Merged at:* {pr_merged_at}\n*URL:* {pr_url}\n*Commit Hash:* {pr_commit_hash}"
+                },
+            },
+        ]
+    }
+
+    response = requests.post(webhook_url, json=message)
+    response.raise_for_status()
+
+# Get the PR data from the environment variables
+pr_title = os.environ['GITHUB_PR_TITLE']
+pr_url = os.environ['GITHUB_PR_LINK']
+pr_merged_at = os.environ['GITHUB_PR_MERGED_AT']
+pr_author = os.environ['GITHUB_ACTOR']
+pr_commit_hash = os.environ['GITHUB_PR_COMMIT_HASH']
+
+# Send PR data to Slack
+slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
+send_to_slack(pr_title, pr_author, pr_merged_at, pr_url, pr_commit_hash, slack_webhook_url)
